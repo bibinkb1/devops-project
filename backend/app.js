e2f1f444f9d5030fbf95f479bac5b7436189f393
bibinkb1 <bibinkb444@gmail.com>
2025-04-18 21:41:23 +0530

    webhook

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, DevOps! this is webhook');
+    res.send('Hello, DevOps! this is new  webhook');
 });
 
 const PORT = process.env.PORT || 5000;
